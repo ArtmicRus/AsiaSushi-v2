9dc958ea79cbd1764b3bc3132df3ee720afc0f5d
ArtmicRus <91434372+ArtmicRus@users.noreply.github.com>
2024-04-12 22:05:58 +0300

    Коннект с базой Postgres и крутой поисковик на сайте

--- app/settings.py
+++ app/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.postgres',
 
     # Дебаг тулбар для джанго
     "debug_toolbar",
@@ -87,8 +88,12 @@ WSGI_APPLICATION = 'app.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'DjangoPrikol',
+        'USER': 'pythonist',
+        'PASSWORD': 'pythonist',
+        'HOST': 'localhost',
+        'PORT': '5433',
     }
 }
 
