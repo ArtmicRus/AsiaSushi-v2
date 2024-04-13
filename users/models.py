44bb1f977b01629b7a8590e3da590442df5e28f3
ArtmicRus <91434372+ArtmicRus@users.noreply.github.com>
2024-04-13 15:02:55 +0300

    Готовая логин форма

--- users/models.py
+++ users/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+
+class User(AbstractUser):
+    image = models.ImageField(upload_to="users_image", blank=True, null=True, verbose_name='Аватар')
+
+    class Meta: 
+        # Название таблицы в БД
+        db_table = 'user'
+        # Имя которые мы хотим в админке (Альтернативное имя) для единственного и множ числа
+        verbose_name = 'Пользователя'
+        verbose_name_plural = 'Пользователи'
+
+    def __str__(self) -> str:
+        return self.username
