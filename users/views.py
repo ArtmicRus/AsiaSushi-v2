406cf800f4a6ac13eb2cef3d40f6fc4dab7bf13b
ArtmicRus <91434372+ArtmicRus@users.noreply.github.com>
2024-04-14 14:23:40 +0300

    Настройка переноса корзины от неавторизованного пользователя к авторизованному

--- users/views.py
+++ users/views.py
@@ -5,6 +5,7 @@ from django.shortcuts import redirect, render
 from django.urls import reverse
 
 
+from carts.models import Cart
 from users.forms import ProfileForm, UserLoginForm, UserRegistrationForm
 
 def login(request):
@@ -16,9 +17,17 @@ def login(request):
             username = request.POST['username']
             password = request.POST['password']
             user = auth.authenticate(username=username, password=password)
+            
+            # Обработка сессионного ключа для того чтобы карзина не потерялась при авторизации
+            session_key = request.session.session_key
+
             if user:
                 auth.login(request, user)
                 messages.success(request, f"{username}, Вы вошли в аккаунт")
+
+                # Перезапись ключа сессии на авторизовавшегося пользователя
+                if session_key:
+                    Cart.objects.filter(session_key=session_key).update(user=user)
 
                 redirect_page = request.POST.get('next', None)
                 if redirect_page and redirect_page != reverse('user:logout'):
@@ -40,8 +49,17 @@ def registration(request):
         form = UserRegistrationForm(data=request.POST)
         if form.is_valid():
             form.save()
+
+            # Обработка сессионного ключа для того чтобы карзина не потерялась при авторизации
+            session_key = request.session.session_key
+
             user = form.instance
             auth.login(request, user)
+
+            # Перезапись ключа сессии на авторизовавшегося пользователя
+            if session_key:
+                Cart.objects.filter(session_key=session_key).update(user=user)
+
             messages.success(request, f"{user.username}, Вы успешно зарегистрированы и вошли в аккаунт")
             return HttpResponseRedirect(reverse('main:index'))
     else:
